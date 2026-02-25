764d98ee39e516582de3e413bb6c108dd870519d
Maria-Bensadon <gabrielabensadon@gmail.com>
2026-02-25 17:49:09 -0300

    Corrección de comentarios en el modulo 3 del TP2

--- Trabajo Practico Nro 02/fs-ejemplo/index.mjs
+++ Trabajo Practico Nro 02/fs-ejemplo/index.mjs
@@ -3,19 +3,19 @@
 
 import fs from 'fs';
 
-// 1. Leer (Sin espacios en './data/...')
+// 1. Leer un archivo de manera asíncrona
 fs.readFile('./data/example.txt', 'utf8', (err, data) => {
     if (err) throw err;
     console.log('Contenido del archivo:', data);
 
 
-    // 2. Escribir (Sin espacios en './data/...')
+    // 2. Escribir en un nuevo archivo
     fs.writeFile('./data/newfile.txt', 'Contenido nuevo.', (err) => {
         if (err) throw err;
         console.log('Archivo creado y escrito');
 
 
-        // 3. Renombrar (Sin espacios en './data/...')
+        // 3. Renombrar un archivo
         fs.rename('./data/newfile.txt', './data/renamedfile.txt', (err) => {
             if (err) throw err;
             console.log('Archivo renombrado');
